9208e47ce0e09d1a799ce4092f7355feb42c1f68
Joaquin Machado <joaquin.machado@ingenieria.uner.edu.ar>
2024-10-31 10:35:59 -0300

    BT terminado

--- firmware/examples/ej_bluetooth_fft/main/ej_bt_dsp_fft.c
+++ firmware/examples/ej_bluetooth_fft/main/ej_bt_dsp_fft.c
@@ -36,8 +36,21 @@
 #define SAMPLE_FREQ	        220
 /*==================[internal data definition]===============================*/
 TaskHandle_t interfaz_task_handle = NULL;
-
+//uint8_t boton;
+char msg[30];
+bool inicio = false;
+uint8_t vida;
+uint8_t progreso;
+uint8_t nivel = 1;
 /*==================[internal functions declaration]=========================*/
+void setBarras(){
+    sprintf(msg, "*P%d", 0); // Actualiza la barra de progreso
+    BleSendString(msg);
+
+    printf(msg, "*V%d", 100);     // Actualiza la barra de vida
+    BleSendString(msg);
+}
+
 /**
  * @brief Función a ejecutarse ante un interrupción de recepción 
  * a través de la conexión BLE.
@@ -46,49 +59,65 @@ TaskHandle_t interfaz_task_handle = NULL;
  * @param length    Longitud del array de datos recibidos
  */
 void read_data(uint8_t * data, uint8_t length){ 
-    printf("cadena:%s\r\n",(char*)data);
-    BleSendString((char*)data);
 
-    switch (data[0]){
-        case 'G': xTaskNotifyGive(interfaz_task_handle); break;
-        case 'R': xTaskNotifyGive(interfaz_task_handle); break;
-        case 'C': xTaskNotifyGive(interfaz_task_handle); break;
+    //sprintf(msg, "%s\r\n", data);   // Guarda en msg el mensaje recibido
+    
+    if(data[0] == 'G'){ // Interrupcion del boton de inicio
+        inicio = true;
+        setBarras();
+        xTaskNotifyGive(interfaz_task_handle);
     }
-    /*
-    if(data[0] == 'G') // Interrupcion del boton de inicio
-        xTaskNotifyGive(interfaz_task_handle);
-    
 
-    if(data[0] == 'g') // Interrupcion del boton de inicio
+    if(data[0] == 'R'){ // Interrupcion del boton de stop
+        inicio = false;
         xTaskNotifyGive(interfaz_task_handle);
-    
+    }
 
-    if(data[0] == 'R') // Interrupcion del boton de stop
+    if(data[0] == '1'){ // Seleccion de nivel
+        nivel = 1;
         xTaskNotifyGive(interfaz_task_handle);
-    */
+    }
+
+    if(data[0] == '2'){ // Seleccion de nivel
+        nivel = 2;
+        xTaskNotifyGive(interfaz_task_handle);
+    }
+
+    if(data[0] == '3'){ // Seleccion de nivel
+        nivel = 3;
+        xTaskNotifyGive(interfaz_task_handle);
+    }
 }
 
 static void manejarInterfaz(void *pvParameter){
     while(true){
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-        printf("Deteccion\n");
 
+        BleSendString(msg); // Muestra el boton tocado en el celular
 
-        BleSendString((char*)"*P");   // Envio el caracter para la barra de progreso
-        //BleSendString((char*)"*V");   // Envio el caracter para la barra de vida
+        if (inicio){
+            progreso = progreso + 10;
+            sprintf(msg, "*P%d", progreso); // Actualiza la barra de progreso
+            BleSendString(msg);
+        }
+        else{
+            vida = vida - 25;   
+            sprintf(msg, "*V%d", vida);     // Actualiza la barra de vida
+            BleSendString(msg);
+        }
     }
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
     ble_config_t ble_configuration = { // Configuracion del BT
-        "JAOQUIN",    // Nombre del dispositivo
+        "JOAQUIN",    // Nombre del dispositivo
         read_data       // Función a ejecutarse ante un interrupción de recepción a través de la conexión BLE.
     };
 
-    xTaskCreate(&manejarInterfaz, "Interfaz", 2048, NULL, 5, &interfaz_task_handle);
+    xTaskCreate(&manejarInterfaz, "Interfaz", 2048, NULL, 5, &interfaz_task_handle); // Crea tarea Interfaz
 
-    LedsInit();  
-    BleInit(&ble_configuration);
+    LedsInit();  // Inicializa LEDs
+    BleInit(&ble_configuration);    // Inicializa BT
     
     while(1){
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
